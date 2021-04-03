92e918ee445293b353a2011cad000c8d62552596
FelipeVasquez350 <felipe.vasquez.lazzara@gmail.com>
2021-04-03 22:04:21 +0200

    Update index.js

--- index.js
+++ index.js
@@ -83,7 +83,7 @@ client.once('ready', () => {
     //jajaja old same old same
     setTimeout(unzip, 1000);
     ThroughDirectory("./CalamityTexturePack");
-    setTimeout(RunPythonScript, 3000);
+    setTimeout(RunPythonScript, 5000);
 });
 
 // Read messages.
